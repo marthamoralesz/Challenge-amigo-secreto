2d3f4d6e9b5442b1b423f0d506214e9cee81bc7d
Azucena Morales <azucenamzarco@gmail.com>
2025-09-03 21:01:50 -0600

    Actualizado - Ya valida si el campo está vacio y sino lo agrega a la lista de amigos

--- CHALLENGE AMIGO SECRETO/app.js
+++ CHALLENGE AMIGO SECRETO/app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaNombreAmigos = [];
+
+function agregarAmigo() {
+    let amigoAgregado = document.getElementById('amigo').value;
+        
+        if (amigoAgregado.trim() == "") {
+            alert ('Por favor, ingresa un nombre');
+        } else {
+            listaNombreAmigos.push(amigoAgregado);
+        }
+
+        console.log(listaNombreAmigos.length)
+        console.log(listaNombreAmigos)
+
+}
